199a74d8659057bbbd774882d2b3834405863db7
jRimbault <jacques.rimbault@gmail.com>
2020-07-23 15:37:50 +0200

    pass the test

--- src/parse.rs
+++ src/parse.rs
@@ -6,9 +6,41 @@ impl std::str::FromStr for ByteSize {
     fn from_str(value: &str) -> Result<Self, Self::Err> {
         match value.parse::<u64>() {
             Ok(v) => return Ok(Self(v)), // simple case, bytes
-            Err(_) => {},
+            Err(_) => {}
         };
-        todo!()
+        let number: String = value
+            .chars()
+            .take_while(|c| c.is_digit(10) || c == &'.')
+            .collect();
+        match number.parse::<f64>() {
+            Ok(v) => {
+                let suffix: String = value
+                    .chars()
+                    .skip_while(|c| c.is_whitespace() || c.is_digit(10) || c == &'.')
+                    .collect();
+                Ok(Self((v * match_suffix(&suffix) as f64) as u64))
+            }
+            Err(error) => Err(format!(
+                "couldn't parse {:?} into a ByteSize, {}",
+                value, error
+            )),
+        }
+    }
+}
+
+fn match_suffix(unit: &str) -> u64 {
+    match unit.to_lowercase().as_str() {
+        "k" | "kb" => super::KB,
+        "ki" | "kib" => super::KIB,
+        "m" | "mb" => super::MB,
+        "mi" | "mib" => super::MIB,
+        "g" | "gb" => super::GB,
+        "gi" | "gib" => super::GIB,
+        "t" | "tb" => super::TB,
+        "ti" | "tib" => super::TIB,
+        "p" | "pb" => super::PB,
+        "pi" | "pib" => super::PIB,
+        _ => 1,
     }
 }
 
@@ -26,7 +58,7 @@ mod tests {
         assert_eq!("0".parse::<ByteSize>().unwrap().0, 0);
         assert_eq!(parse("0"), 0);
         assert_eq!(parse("500"), 500);
-        assert_eq!(parse("1K"), 1 * MB);
+        assert_eq!(parse("1K"), 1 * KB);
         assert_eq!(parse("1Ki"), 1 * KIB);
         assert_eq!(parse("1.5Ki"), (1.5 * KIB as f64) as u64);
         assert_eq!(parse("1KiB"), 1 * KIB);
@@ -38,6 +70,6 @@ mod tests {
         assert_eq!(parse("88TB"), 88 * TB);
         assert_eq!(parse("521TiB"), 521 * TIB);
         assert_eq!(parse("8 PB"), 8 * PB);
-        assert_eq!(parse("12 PB"), 12 * PIB);
+        assert_eq!(parse("12 PiB"), 12 * PIB);
     }
 }
